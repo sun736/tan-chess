5dc16c5e0c31865e9818f7cd4260270831f1ddd8
Kaiqi <thats2ez@gmail.com>
2014-10-17 14:29:15 -0700

    [enhance] add helper function

---
+++ WildPiece/WildPiece/Helper.swift
@@ -0,0 +1,16 @@
+//
+//  Helper.swift
+//  WildPiece
+//
+//  Created by Kaiqi on 14/10/17.
+//  Copyright (c) 2014年 Project TC. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension CGVector {
+    func projectionOn(unit : CGVector) -> CGVector {
+        return self
+    }
+}
