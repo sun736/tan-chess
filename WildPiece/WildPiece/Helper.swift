ed9ecfe3aae3ccdcced6951288dafd19189f5258
Kaiqi <thats2ez@gmail.com>
2014-10-17 15:04:54 -0700

    [feature] vector projection function

--- WildPiece/WildPiece/Helper.swift
+++ WildPiece/WildPiece/Helper.swift
@@ -11,6 +11,13 @@ import SpriteKit
 
 extension CGVector {
     func projectionOn(unit : CGVector) -> CGVector {
-        return self
+        // a - unit vector, b - original vector, x - result projection
+        let aLength = CGFloat(hypotf(Float(unit.dx), Float(unit.dy)))
+        let dotProduct = self.dx * unit.dx + self.dy * unit.dy
+        let bLength = CGFloat(hypotf(Float(self.dx), Float(self.dy)))
+        let xLength = dotProduct / aLength
+        let scaleFactor = xLength / aLength
+        
+        return CGVectorMake(unit.dx * scaleFactor, unit.dy * scaleFactor)
     }
 }
