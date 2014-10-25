7046c59af4b15dff8a7ff650aa5fc9311f9b04fb
YixiangLiu <yixiang@usc.edu>
2014-10-25 15:13:47 -0700

    [Fix] Bring board to bottom

--- WildPiece/WildPiece/GameScene.swift
+++ WildPiece/WildPiece/GameScene.swift
@@ -202,6 +202,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, LogicDelegate {
         
         let background = SKSpriteNode(imageNamed: "board")
         background.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        background.zPosition = -10.0
         scene?.addChild(background)
         /*SKTexture *backgroundTexture = [SKTexture textureWithImageNamed:@"MyImage.png"];
         SKSpriteNode *background = [SKSpriteNode spriteNodeWithTexture:backgroundTexture size:self.view.frame.size];
